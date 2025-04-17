5a93efd75c26a3068394ac5eb19626a92937c74c
NguyenThiBac2005 <nguyenthibacc2005@gmail.com>
2025-04-17 23:52:07 +0700

    up dambighet.htmlhtml

--- html/vanhoc/sach/script.js
+++
@@ -1,7 +0,0 @@
-const toggleBtn = document.getElementById("toggleBtn");
-const moreInfo = document.getElementById("moreInfo");
-
-toggleBtn.addEventListener("click", () => {
-  moreInfo.classList.toggle("hidden");
-  toggleBtn.textContent = moreInfo.classList.contains("hidden") ? "Xem thêm" : "Ẩn bớt";
-});
